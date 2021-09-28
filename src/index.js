4a7e1d837105f700e31c33c62490bd3cc0cd7c5d
trishacho <89955497+trishacho@users.noreply.github.com>
2021-09-28 17:28:19 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './Example';
-
-ReactDOM.render(<Example />, document.getElementById('root'));
-
-// using Node.js to execute the JavaScript code
